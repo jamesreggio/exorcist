5ca0ef211c80e9b36fa2d189b3e84c02c0b4862b
James Reggio <james.reggio@stripe.com>
2015-03-02 18:25:01 -0800

    Add tests from @gionkunz branch

--- test/exorcist.js
+++ test/exorcist.js
@@ -7,20 +7,22 @@ var through  = require('through2')
 var exorcist = require('../')
 
 var fixtures = __dirname + '/fixtures';
-var mapfile = fixtures + '/bundle.js.map';
+var scriptMapfile = fixtures + '/bundle.js.map';
+var styleMapfile = fixtures + '/to.css.map';
 
 // This base path is baken into the source maps in the fixtures.
 var base = '/Users/thlorenz/dev/projects/exorcist';
 
 function setup() {
-  if (fs.existsSync(mapfile)) fs.unlinkSync(mapfile);
+  if (fs.existsSync(scriptMapfile)) fs.unlinkSync(scriptMapfile);
+  if (fs.existsSync(styleMapfile)) fs.unlinkSync(styleMapfile);
 }
 
 test('\nwhen piping a bundle generated with browserify through exorcist without adjusting properties', function (t) {
   setup();
   var data = ''
   fs.createReadStream(fixtures + '/bundle.js', 'utf8')
-    .pipe(exorcist(mapfile))
+    .pipe(exorcist(scriptMapfile))
     .pipe(through(onread, onflush));
 
     function onread(d, _, cb) { data += d; cb(); }
@@ -30,7 +32,7 @@ test('\nwhen piping a bundle generated with browserify through exorcist without 
       t.equal(lines.length, 27, 'pipes entire bundle including prelude, sources and source map url')
       t.equal(lines.pop(), '//# sourceMappingURL=bundle.js.map', 'last line as source map url pointing to .js.map file')
 
-      var map = JSON.parse(fs.readFileSync(mapfile, 'utf8'));
+      var map = JSON.parse(fs.readFileSync(scriptMapfile, 'utf8'));
       t.equal(map.file, 'generated.js', 'leaves file name unchanged')
       t.equal(map.sources.length, 4, 'maps 4 source files')
       t.equal(map.sources[0].indexOf(base), 0, 'uses absolute source paths')
@@ -47,7 +49,7 @@ test('\nwhen piping a bundle generated with browserify through exorcist and adju
   setup();
   var data = ''
   fs.createReadStream(fixtures + '/bundle.js', 'utf8')
-    .pipe(exorcist(mapfile, 'http://my.awseome.site/bundle.js.map'))
+    .pipe(exorcist(scriptMapfile, 'http://my.awseome.site/bundle.js.map'))
     .pipe(through(onread, onflush));
 
     function onread(d, _, cb) { data += d; cb(); }
@@ -57,7 +59,7 @@ test('\nwhen piping a bundle generated with browserify through exorcist and adju
       t.equal(lines.length, 27, 'pipes entire bundle including prelude, sources and source map url')
       t.equal(lines.pop(), '//# sourceMappingURL=http://my.awseome.site/bundle.js.map', 'last line as source map url pointing to .js.map file at url set to supplied url')
 
-      var map = JSON.parse(fs.readFileSync(mapfile, 'utf8'));
+      var map = JSON.parse(fs.readFileSync(scriptMapfile, 'utf8'));
       t.equal(map.file, 'generated.js', 'leaves file name unchanged')
       t.equal(map.sources.length, 4, 'maps 4 source files')
       t.equal(map.sourcesContent.length, 4, 'includes 4 source contents')
@@ -73,7 +75,7 @@ test('\nwhen piping a bundle generated with browserify through exorcist and adju
   setup();
   var data = ''
   fs.createReadStream(fixtures + '/bundle.js', 'utf8')
-    .pipe(exorcist(mapfile, 'http://my.awseome.site/bundle.js.map', 'http://my.awesome.site/src'))
+    .pipe(exorcist(scriptMapfile, 'http://my.awseome.site/bundle.js.map', 'http://my.awesome.site/src'))
     .pipe(through(onread, onflush));
 
     function onread(d, _, cb) { data += d; cb(); }
@@ -83,7 +85,7 @@ test('\nwhen piping a bundle generated with browserify through exorcist and adju
       t.equal(lines.length, 27, 'pipes entire bundle including prelude, sources and source map url')
       t.equal(lines.pop(), '//# sourceMappingURL=http://my.awseome.site/bundle.js.map', 'last line as source map url pointing to .js.map file at url set to supplied url')
 
-      var map = JSON.parse(fs.readFileSync(mapfile, 'utf8'));
+      var map = JSON.parse(fs.readFileSync(scriptMapfile, 'utf8'));
       t.equal(map.file, 'generated.js', 'leaves file name unchanged')
       t.equal(map.sources.length, 4, 'maps 4 source files')
       t.equal(map.sourcesContent.length, 4, 'includes 4 source contents')
@@ -99,7 +101,7 @@ test('\nwhen piping a bundle generated with browserify through exorcist and adju
   setup();
   var data = ''
   fs.createReadStream(fixtures + '/bundle.js', 'utf8')
-    .pipe(exorcist(mapfile, 'http://my.awseome.site/bundle.js.map', 'http://my.awesome.site/src', base))
+    .pipe(exorcist(scriptMapfile, 'http://my.awseome.site/bundle.js.map', 'http://my.awesome.site/src', base))
     .pipe(through(onread, onflush));
 
     function onread(d, _, cb) { data += d; cb(); }
@@ -109,7 +111,7 @@ test('\nwhen piping a bundle generated with browserify through exorcist and adju
       t.equal(lines.length, 27, 'pipes entire bundle including prelude, sources and source map url')
       t.equal(lines.pop(), '//# sourceMappingURL=http://my.awseome.site/bundle.js.map', 'last line as source map url pointing to .js.map file at url set to supplied url')
 
-      var map = JSON.parse(fs.readFileSync(mapfile, 'utf8'));
+      var map = JSON.parse(fs.readFileSync(scriptMapfile, 'utf8'));
       t.equal(map.file, 'generated.js', 'leaves file name unchanged')
       t.equal(map.sources.length, 4, 'maps 4 source files')
       t.equal(map.sources[0].indexOf(base), -1, 'uses relative source paths')
@@ -127,7 +129,7 @@ test('\nwhen piping a bundle generated with browserify thats missing a map throu
   var data = ''
   var missingMapEmitted = false;
   fs.createReadStream(fixtures + '/bundle.nomap.js', 'utf8')
-    .pipe(exorcist(mapfile))
+    .pipe(exorcist(scriptMapfile))
     .on('missing-map', function () { missingMapEmitted = true })
     .pipe(through(onread, onflush));
 
@@ -141,4 +143,30 @@ test('\nwhen piping a bundle generated with browserify thats missing a map throu
       cb();
       t.end()
     }
+})
+
+test('\nwhen performing a stylish exorcism', function (t) {
+  setup();
+  var data = ''
+  fs.createReadStream(fixtures + '/to.css', 'utf8')
+    .pipe(exorcist(styleMapfile))
+    .pipe(through(onread, onflush));
+
+  function onread(d, _, cb) { data += d; cb(); }
+
+  function onflush(cb) {
+    var lines = data.split('\n')
+    t.equal(lines.length, 23, 'pipes entire style including prelude, sources and source map url')
+    t.equal(lines.pop(), '/*# sourceMappingURL=to.css.map */', 'last line as source map url pointing to .css.map file')
+
+    var map = JSON.parse(fs.readFileSync(styleMapfile, 'utf8'));
+    t.equal(map.file, 'to.css', 'leaves file name unchanged')
+    t.equal(map.sources.length, 2, 'maps 4 source files')
+    t.equal(map.sourcesContent.length, 2, 'includes 4 source contents')
+    t.equal(map.mappings.length, 214, 'maintains mappings')
+    t.equal(map.sourceRoot, '', 'leaves source root an empty string')
+
+    cb();
+    t.end();
+  }
 })
